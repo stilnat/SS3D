eeb7198e51883e68d26415a1f8c1f18eaba04db9
stilnat <nathan.raynal.castang@gmail.com>
2024-12-05 16:01:11 +0100

    better slider

--- Assets/Scripts/SS3D/Systems/Atmospherics/AtmosRework/MixerView.cs
+++ Assets/Scripts/SS3D/Systems/Atmospherics/AtmosRework/MixerView.cs
@@ -26,53 +26,27 @@ public class MixerView : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI _displayTargetPressure;
 
-    private bool _dirtyTargetPressure;
-
-    private bool _dirtyFirstInput;
-
-    private const float SliderUpdateTime = 0.1f;
-
-    private float _previousPressureUpdateTime;
-
-    private float _previousInputUpdateTime;
-
-    private void Update()
-    {
-        if (_dirtyTargetPressure && Time.time - _previousPressureUpdateTime > SliderUpdateTime)
-        {
-            _mixerAtmosObject.SetTargetPressure(_sliderTargetPressure.value);
-            _dirtyTargetPressure = false;
-            _previousPressureUpdateTime = Time.time;
-        }
-
-        if (_dirtyFirstInput && Time.time - _previousInputUpdateTime > SliderUpdateTime)
-        {
-            _mixerAtmosObject.SetFirstInput(_sliderFirstInput.value);
-            _dirtyFirstInput = false;
-            _previousInputUpdateTime = Time.time;
-        }
-    }
 
     public void Initialize(MixerAtmosObject mixerAtmosObject)
     {
         _mixerAtmosObject = mixerAtmosObject;
         _turnOn.Switch += isOn => _mixerAtmosObject.SetMixerActive(isOn);
-        _sliderFirstInput.onValueChanged.AddListener(UpdateMixerFirstInputAmount);
-        _sliderTargetPressure.onValueChanged.AddListener(UpdateMixerTargetPressure);
+        _sliderFirstInput.ValueTickUpdated += UpdateMixerFirstInputAmount;
+        _sliderTargetPressure.ValueTickUpdated += UpdateMixerTargetPressure;
         _mixerAtmosObject.UpdateMixerFirstInputAmount += amount => UpdateVisualFirstInputAmount(amount, false);
         _mixerAtmosObject.UpdateTargetPressure += amount => UpdateVisualTargetPressure(amount, false);
     }
 
     private void UpdateMixerFirstInputAmount(float amount)
     {
+        _mixerAtmosObject.SetFirstInput(amount);
         UpdateVisualFirstInputAmount(amount, true);
-        _dirtyFirstInput = true;
     }
 
     private void UpdateMixerTargetPressure(float amount)
     {
+        _mixerAtmosObject.SetTargetPressure(amount);
         UpdateVisualTargetPressure(amount, true);
-        _dirtyTargetPressure = true;
     }
 
     private void UpdateVisualFirstInputAmount(float amount, bool fromUI)
